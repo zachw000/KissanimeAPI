f2fe04e2b40ab905ea50793feedf581bf55dd7eb
Zachary Waldron <wewingames222@gmail.com>
2016-10-26 22:19:01 -0700

    Added License

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 var request = require('request');
 const EventEmitter = require('events');
 /** @module connection/cookie */
@@ -5,27 +6,27 @@ const EventEmitter = require('events');
 class KissanimeEvent extends EventEmitter {}
 const kissanimeEvent = new KissanimeEvent();
 
-module.exports.useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36";
-module.exports.jar = request.jar();
-
 module.exports = {
+    useragent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36",
+    jar: request.jar(),
     /**
      * Attempts connection to kissanime.to.
-     * @param {string} [useragent] - User-Agent string to send to kissanime for verification.
+     * @param {string} [userAgent] - User-Agent string to send to kissanime for verification.
      * @return {Object} cookie
      * @description Returns a session cookie
      */
-    connect: function(useragent = null) {
+    connect: function(userAgent = null) {
         /**
          * @todo Implement request features
          * @todo Create non-blocking code for other module loading
          */
-        useragent = useragent !== null ? useragent :
+        this.useragent = userAgent !== null ? userAgent :
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36';
-        kissanimeEvent.emit("connect", useragent);
+        kissanimeEvent.emit("connect", this.useragent);
     }
 };
 
+// Connect to kissanime and grab auth data.
 kissanimeEvent.on("connect", function(useragent) {
     console.log("Connection Request Called");
     request({
@@ -38,7 +39,8 @@ kissanimeEvent.on("connect", function(useragent) {
         },
         jar: module.exports.jar
     }, function (error, response, body) {
-        module.exports.useragent = useragent;
-        console.log(body);
+        // Verify User-Agent string
+        console.log(module.exports.useragent);
+
     });
 });
